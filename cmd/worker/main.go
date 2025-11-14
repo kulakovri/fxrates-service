df55375c0a303790df2bf9f58d67c6c6c1c956f3
Roman Kulakov <kulakovri@gmail.com>
2025-11-14 14:42:34 +0100

    move WorkerType from worker init

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	"fxrates-service/internal/bootstrap"
-	"fxrates-service/internal/config"
 	"fxrates-service/internal/infrastructure/logx"
 	"go.uber.org/zap"
 )
@@ -12,25 +11,12 @@ import (
 func main() {
 	log := logx.L()
 	ctx := context.Background()
-	switch config.Load().WorkerType {
-	case "grpc":
-		run, cleanup, err := bootstrap.InitGRPCRunner(ctx)
-		if err != nil {
-			log.Fatal("init grpc runner", zap.Error(err))
-		}
-		defer cleanup()
-		if err := run(ctx); err != nil {
-			log.Fatal("grpc worker server exited", zap.Error(err))
-		}
-	default:
-		w, cleanup, err := bootstrap.InitDBWorker(ctx)
-		if err != nil {
-			log.Fatal("init db worker", zap.Error(err))
-		}
-		defer cleanup()
-		if w == nil {
-			log.Fatal("no worker configured (WORKER_TYPE)", zap.Error(err))
-		}
-		w.Start(ctx)
+	run, cleanup, err := bootstrap.InitWorkerApp(ctx)
+	if err != nil {
+		log.Fatal("init worker app", zap.Error(err))
+	}
+	defer cleanup()
+	if err := run(ctx); err != nil {
+		log.Fatal("worker exited", zap.Error(err))
 	}
 }
